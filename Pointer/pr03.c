1a821af4636df46f279f16eb667b22e43a036524
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-12 22:42:16 +0530

    Basic progam

---
+++ Pointer/pr03.c
@@ -0,0 +1,10 @@
+//for understanding purpose basic progam
+#include<stdio.h>
+void main(){
+        int x=10;
+        int *ptr=&x;
+        printf("value of x:%d\n",x);
+        printf("adress of x:%p\n",&x);
+        printf("value in ptr box:%p\n",ptr);
+        printf("adress of ptr box:%p\n",&ptr);
+ }
