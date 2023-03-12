0e3b01ca81a31bf11f118d5754b4225fd125e64c
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-12 22:36:59 +0530

    Intro  progrm of pointer

---
+++ Pointer/pr01.c
@@ -0,0 +1,8 @@
+//Intro  progrm of pointer
+#include<stdio.h>
+void main(){
+        int x=10;
+        printf("value of x:%d\n",x);
+        printf("Address of x:%p\n",&x);
+       printf("last:%d\n",&x);
+}
