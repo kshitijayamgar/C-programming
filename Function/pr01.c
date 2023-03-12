2bbff4d94187ec1fa16e015317761ea2e74cdfde
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-12 22:10:59 +0530

    Basic prg of function

--- Function/pr01.c
+++ Function/pr01.c
@@ -1,1 +1,10 @@
-
+//Basic flow of function
+#include<stdio.h>
+work(){
+        printf("working\n");
+}
+void main(){
+        printf("start main\n");
+            work();
+        printf("end main\n");
+}
