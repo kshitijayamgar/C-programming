7ed026568c5735ae61030a142d5218da2b637fb4
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-17 19:57:07 +0530

    ptrn04.c

---
+++ pattern/ptrn04.c
@@ -0,0 +1,18 @@
+/*  A A A
+    B B B
+    C C C
+           */
+
+#include<stdio.h>
+void main(){
+        char ch='A';
+  for(int i=1;i<=3;i++){
+    for(int j=1;j<=3;j++){
+      printf(" %c ",ch);
+    }
+    ch++;
+    printf("\n");
+  }
+  printf("\n");
+}
+
