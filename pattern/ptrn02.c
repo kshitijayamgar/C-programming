d32a3556fc92a6f7a196510796b14cb4e6bec795
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-17 19:49:45 +0530

    square pattren of 'a' letter

---
+++ pattern/ptrn02.c
@@ -0,0 +1,15 @@
+/*  a a a
+    a a a
+    a a a
+           */
+
+#include<stdio.h>
+void main(){
+  for(int i=1;i<=3;i++){
+    for(int j=1;j<=3;j++){
+      printf(" a ");
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
