0cc841aa1d2db6cf6adbfc63f6aa9b7281fdc6cf
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-18 22:18:24 +0530

    ptrn11.c

---
+++ pattern/ptrn11.c
@@ -0,0 +1,21 @@
+/*
+  1 4 9
+  1 4 9
+  1 4 9
+ 
+ */
+#include<stdio.h>
+void main(){
+        
+        for(int i=1;i<=3;i++){
+
+          for(int j=1;j<=3;j++){
+                  printf(" %d ",j*j);
+         
+            }
+
+          printf("\n");
+        }
+        printf("\n");
+}
+
