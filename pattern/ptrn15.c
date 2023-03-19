afd55dae5c47fbc82734df2d7a157a0d1848a87a
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-19 19:45:04 +0530

    ptrn15.c

---
+++ pattern/ptrn15.c
@@ -0,0 +1,19 @@
+/*
+  A A A
+  B B B
+  C C C
+ 
+ */
+#include<stdio.h>
+void main(){
+     int ch= 65;
+     for(int i=1;i<=3;i++){
+	for(int j=1;j<=3;j++){
+	 printf(" %c ",ch);
+	}
+	ch++;
+	printf("\n");
+     }
+     printf("\n");
+   
+}
