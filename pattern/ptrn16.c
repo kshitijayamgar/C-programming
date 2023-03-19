fa256ea3916d826367f103fc31f47141fc9631f6
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-19 19:45:34 +0530

    ptrn16.c

---
+++ pattern/ptrn16.c
@@ -0,0 +1,22 @@
+/*
+  A B C
+  A B C
+  A B C
+ 
+ */
+#include<stdio.h>
+void main(){
+	
+	for(int i=1;i<=3;i++){
+		int ch=65;
+	    for(int j=1;j<=3;j++){
+                 
+            printf(" %c ",ch);
+	  
+	  ch++;
+	 }
+	    printf("\n");
+      }
+	
+	printf("\n");
+}
