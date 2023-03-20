3d1088201586a0d00f123590d86a7cee300ebe16
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-20 21:48:56 +0530

    ptrn17.c

---
+++ pattern/ptrn17.c
@@ -0,0 +1,23 @@
+/*
+  a b c
+  a b c
+  a b c
+ 
+ */
+#include<stdio.h>
+void main(){
+
+        for(int i=1;i<=3;i++){
+                int ch=97;
+            for(int j=1;j<=3;j++){
+
+            printf(" %c ",ch);
+
+          ch++;
+         }
+            printf("\n");
+      }
+
+        printf("\n");
+}
+
