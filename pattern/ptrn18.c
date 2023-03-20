eee0223f20c60b2beb6fc87cb1e7c88ae49d5ceb
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-20 21:49:43 +0530

    ptrn18.c

---
+++ pattern/ptrn18.c
@@ -0,0 +1,20 @@
+/*
+A A A A A
+B B B B
+C C C
+D D
+E
+*/
+#include<stdio.h>
+void main(){
+	char ch='A';
+	for(int i=1;i<=5;i++){
+	  for(int j=5;j>=i;j--){
+	   printf(" %c ",ch);
+	  }
+	  ch++;
+	  printf("\n");
+      }
+	printf("\n");
+}
+
