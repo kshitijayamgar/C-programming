c75d13af66f284520ab30359da736df0452d63f1
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-23 15:47:35 +0530

    ptrn28.c

---
+++ pattern/ptrn28.c
@@ -0,0 +1,19 @@
+/*
+  
+  * * * *
+  * * *
+  * * 
+  * 
+
+  */
+#include<stdio.h>
+void main(){
+        for(int i=1;i<=4;i++){
+          for(int j=4;j>=i;j--){
+                printf(" * ");
+          }
+          printf("\n");
+        }
+        printf("\n");
+}
+
