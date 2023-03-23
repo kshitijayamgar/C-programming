7416670cb6b69e7a3f444b9c74a95cbb3643f1e4
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-23 15:37:23 +0530

    ptrn25.c

---
+++ pattern/ptrn25.c
@@ -0,0 +1,19 @@
+/*
+  1 4 9
+  1 4 9
+  1 4 9
+
+  */
+#include<stdio.h>
+void main(){
+	for(int i=1;i<=3;i++){
+	  //int x=1; we can also use extra variable here
+	 for(int j=1;j<=3;j++){
+	    printf(" %d ",j*j);  //printf(" %d ",x*x);
+
+	  //  x++; 
+	 }
+	 printf("\n");
+    }
+	printf("\n");
+}
