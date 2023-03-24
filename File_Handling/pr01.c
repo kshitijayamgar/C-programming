33a20ff33f5e73607453ceb6ecfbde979aba4b66
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-24 22:58:08 +0530

    pr01.c

--- File_Handling/pr01.c
+++ File_Handling/pr01.c
@@ -1,1 +1,12 @@
+//program for create file using file handling function fopen()
 
+#include<stdio.h>
+void main(){
+        FILE *fp=fopen("basic.txt","w");
+        printf("%p\n",fp);
+}
+/*  
+    In this program we use fopen() function for open the file,
+    and with " W " write mode if the file dosent exist this fopen() with write mode also create the file.
+    
+    */
