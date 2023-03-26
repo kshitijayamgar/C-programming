ed266906cefe3177bd929af03807a970d5bc9ffd
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-26 19:32:38 +0530

    pr03.c

---
+++ String in C/pr03.c
@@ -0,0 +1,8 @@
+//Basic program for taking inputs from user for string
+#include<stdio.h>
+void main(){
+        char name[10];
+        printf("enter name:\n");
+        scanf("%s",name);
+        printf("%s\n",name);
+}
