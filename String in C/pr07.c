1358fa2df95d754c274e7448c42af8ab85a07e5d
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-28 22:48:18 +0530

    pr07.c

---
+++ String in C/pr07.c
@@ -0,0 +1,9 @@
+//Use gets puts in string program
+#include<stdio.h>                                
+void main(){
+        char arr[10];
+        printf("enter string:\n");
+        gets(arr);
+        puts(arr);
+}
+//output with warning for gets() function ignore it for now we will find solution on that also in next programs
