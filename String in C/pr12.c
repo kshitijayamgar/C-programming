bc4bfae3302c3af882881307e8a7ef991db451bc
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-03-30 15:27:32 +0530

    pr12.c

---
+++ String in C/pr12.c
@@ -0,0 +1,23 @@
+//library function strcpy
+
+#include<string.h>
+
+#include<stdio.h>
+
+void main(){
+
+        char *str1="rudra";
+
+        char str2[10];
+
+        puts(str1);
+
+        puts(str2);
+
+        strcpy(str1,str2);
+
+        puts(str1);
+
+        puts(str2);
+
+}
