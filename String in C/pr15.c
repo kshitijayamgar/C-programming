79d4af226ac5a2be8f11ac2b6757d0dc71814680
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-02 07:14:46 +0530

    pr15.c

---
+++ String in C/pr15.c
@@ -0,0 +1,13 @@
+//library function strcat
+#include<string.h>
+#include<stdio.h>
+void main(){
+        char str1[10]={'s','h','a','s','h','i'};
+        char *str2="kant";
+        puts(str1);
+        puts(str2);
+       strcat(str1,str2);
+        puts(str1);
+        puts(str2);
+
+}
