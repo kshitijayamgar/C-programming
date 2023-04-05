d256248eafc0437aee0dae9bc2bc220adbf50622
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-05 19:12:21 +0530

    pr20.c

---
+++ String in C/pr20.c
@@ -0,0 +1,20 @@
+//own function for changing the string to  lower case
+
+#include<stdio.h>
+char * mystrlwr(char *str){
+        while(*str!='\0'){
+                if(*str>='A' && *str<='Z'){
+                *str=*str+32;
+                }
+                str++;
+        }
+
+        return str;
+}
+void main(){
+       char str[20]={'A','S','H','O','K','\0'};
+ //char *str="ASHAK"; do not try change this type of string to  the lowercase you will get  segmentation  fault
+        puts(str);
+        mystrlwr(str);
+        puts(str);
+}
