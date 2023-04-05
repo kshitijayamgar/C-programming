4d5e1ed829a1dcca27c2fd252d3d73d8faea9b86
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-05 19:17:10 +0530

    pr21.c
    To uppercase( )

---
+++ String in C/pr21.c
@@ -0,0 +1,18 @@
+//own function for change the string to upper case 
+//mystrupr
+#include<stdio.h>
+char * mystrupr(char *str){
+        while(*str!='\0'){
+                if(*str>=97&&*str<=122){
+                        *str=*str-32;
+                }
+                str++;
+          }
+        return str;
+}
+void main(){
+        char str[]={'g','e','e','t','a','\0'};
+        puts(str);
+        mystrupr(str);
+        puts(str);
+}
