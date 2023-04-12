ec9d932b838defeb4f95447979d047955ba84200
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-12 12:48:28 +0530

    Add files via upload

---
+++ pattern/ptrn31.c
@@ -0,0 +1,33 @@
+/*
+ 
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+int  m = 10;
+for(int i = 1;i <= 5;i++)
+{
+for(int j = 1;j < i;j++)
+{
+printf(" ");
+}
+for(int k = 1;k < m;k++)
+{
+printf("*");
+}
+m = m - 2;
+printf("\n");
+}
+return 0;
+}
