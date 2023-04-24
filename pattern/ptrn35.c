1e1fdbf3be5da489388fbaa5108ca70aa9232e25
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-24 21:37:23 +0530

    ptrn35.c

---
+++ pattern/ptrn35.c
@@ -0,0 +1,30 @@
+/*
+     1 
+   1 2 
+  1 2 3 
+ 1 2 3 4 
+1 2 3 4 5 
+
+*/
+
+#include<stdio.h>
+
+void  main()
+{
+  int n;
+ printf("Enter the number of rows: ");
+ scanf("%d",&n);
+ for(int i = 1; i <= n; i++)
+  {
+    for(int j = n; j > i; j--)
+    {
+      printf(" ");
+     }
+for(int j = 1; j <= i; j++)
+    {
+    printf("%d ",j);
+   }
+    printf("\n");
+    }
+
+}
