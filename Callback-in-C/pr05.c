18dea08d0c6b116491fba73d26a1027cf2d5a5ce
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-06-07 12:02:26 +0530

    Add files via upload

---
+++ Callback-in-C/pr05.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void my_function(){
+
+   printf("This is a normal function.\n");
+}
+
+void my_callback_function(void (*ptr)()) {
+ 
+    printf("This is callback function.\n");
+
+   (*ptr)();    //calling the callback function
+}
+
+void main() {
+   
+  void (*ptr)() = &my_function;
+  
+  my_callback_function(ptr);
+ }
