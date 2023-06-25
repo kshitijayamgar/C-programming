735ded7490d3b78db09db57a74b7e09acd8441ce
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-06-25 14:03:06 +0530

    Add files via upload

---
+++ Must-solve-programs/pr01.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>  
+     int main()    
+    {    
+    int n,sum=0,m;    
+    printf("Enter a number:");    
+    scanf("%d",&n);    
+    while(n>0)    
+    {    
+    m=n%10;    
+    sum=sum+m;    
+    n=n/10;    
+    }    
+    printf("Sum of entered num is=%d\n",sum);    
+    return 0;  
+    }   
