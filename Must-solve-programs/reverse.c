c39bd317ea86c103f9d295bba4bd12682052198e
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-07-04 13:02:59 +0530

    Create reverse.c

---
+++ Must-solve-programs/reverse.c
@@ -0,0 +1,13 @@
+    #include<stdio.h>  
+     void main(){    
+    int n, reverse=0, rem;    
+    printf("Enter a number: ");    
+      scanf("%d", &n);    
+      while(n!=0){    
+         rem=n%10;    
+         reverse=reverse*10+rem;    
+         n/=10;    
+      }    
+    printf("Reversed Number: %d",reverse);    
+    return 0;  
+    }   
